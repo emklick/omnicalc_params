5f1826ed92c296956004cd201e71cd8afe300556
Em Klick <eklick@chicagobooth.edu>
2017-07-17 00:31:26 +0000

    setup and forms/templates

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -9,11 +9,51 @@ class CalculationsController < ApplicationController
     render("calculations/flexible_square_template.html.erb")
   end
   
-  def square_form
-      
-      
-     render("calculations/square_form_template.html.erb") 
+  def flex_square_root
+    # The incoming parameters for this action look like {"a_number"=>"5"}
+    # Rails stores that hash in a variable called params
+    
+    @user_number = params["a_number"].to_i
+    @squared_root = Math.sqrt(@user_number)
+    
+    render("calculations/flexible_square_template.html.erb")
   end
+  
+  def square_form
+    
+    render("calculations/square_form_template.html.erb")
+  end
+  
+  def process_square
+    # The incoming parameters for this action look like {"the_user_number"=>"5"}
+    # Rails stores that hash in a variable called params
+    
+    @user_number = params["the_user_number"].to_i
+    @squared_number = @user_number**2
+    
+    render("calculations/square_results_template.html.erb")
+  end
+  
+  def payment_form
+   
+    interest_rate/years/present_value
+    
+    # @apr = params["interest_rate"].to_i
+    # @number_of_years = 
+    # @principal = 
+    
+    
+    # monthly_interest = (@apr/12)/100
+    # number_of_monthly_payments = @years*-12
+
+    # @monthly_payment = @principal*(monthly_interest/(1-(1+monthly_interest)**number_of_monthly_payments))
+    
+    render("calculations/payment_form_template.html.erb")
+  end
+  
+  
+  
+  
   
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,20 @@ Rails.application.routes.draw do
   
   get("/flexible/square/:a_number", { :controller => "calculations", :action => "flex_square" })
   
+  get("/flexible/square_root/:a_number", { :controller => "calculations", :action => "flex_square_root" })
+  
   get("/square/new", { :controller => "calculations", :action => "square_form" })
+  
+  get("/square/results", { :controller => "calculations", :action => "process_square" })
+   
+  get("/payment/interest_rate/years/present_value", { :controller => "calculations", :action => "payment_form" })
+  
+  get("/random/results", { :controller => "calculations", :action => "random_form" })
+  
+  get("/word_count/results", { :controller => "calculations", :action => "process_wordcount" })
+  
+  get("/descriptive_stats/results", { :controller => "calculations", :action => "process_stats" })
+  
   
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
