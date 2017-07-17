bfbf802e249f6a282538a16deb4438f2788d3aaa
Em Klick <eklick@chicagobooth.edu>
2017-07-17 00:56:47 +0000

    forms and results

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -10,14 +10,37 @@ class CalculationsController < ApplicationController
   end
   
   def flex_square_root
-    # The incoming parameters for this action look like {"a_number"=>"5"}
-    # Rails stores that hash in a variable called params
     
     @user_number = params["a_number"].to_i
     @squared_root = Math.sqrt(@user_number)
     
     render("calculations/flexible_square_template.html.erb")
   end
+  
+   def flex_payment
+    
+    @monthly_interest = (params["a_number"].to_i)
+    @number_of_monthly_payments = @years*-12
+
+    @monthly_payment = @principal*(monthly_interest/(1-(1+monthly_interest)**number_of_monthly_payments))
+    
+    render("calculations/flexible_square_template.html.erb")
+  end
+  
+   def flex_random_number
+    
+    @random_number = rand(100)-rand(50)
+    
+    # random number between 50 & 100
+    
+    render("calculations/flexible_square_template.html.erb")
+  end
+  
+  
+  
+  
+  
+  
   
   def square_form
     
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,17 @@ Rails.application.routes.draw do
   
   get("/flexible/square_root/:a_number", { :controller => "calculations", :action => "flex_square_root" })
   
+  get("/flexible/payment/:interest_rate/:years/:principal/", { :controller => "calculations", :action => "flex_payment" })
+  
+  get("/flexible/random/:@random_number", { :controller => "calculations", :action => "flex_random" })
+  
+  
+  
   get("/square/new", { :controller => "calculations", :action => "square_form" })
   
-  get("/square/results", { :controller => "calculations", :action => "process_square" })
+  get("/square/results", { :controller => "calculations", :action => "process_square_root_form" })
+   
+  get("/square/results", { :controller => "calculations", :action => "process_square_root_form" })
    
   get("/payment/interest_rate/years/present_value", { :controller => "calculations", :action => "payment_form" })
   
