2eb9375c4f5e049f0808481f63d270636f46474f
Em Klick <eklick@chicagobooth.edu>
2017-07-18 00:51:15 +0000

    qwertyy

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -62,33 +62,15 @@ class CalculationsController < ApplicationController
     render("calculations/square_form_template.html.erb")
   end
   
-  def square_results
-    
-    @user_number_root = params["a_number"].to_i
-    @squared_number = @user_number_root**2
-    
-    render("calculations/square_results_template.html.erb")
-  end
-  
-  
   
   def square_root_form
     
-    @user_number_root = params["square_root_number"].to_i
+    @user_number_square_root = params["square_root_number"].to_i
     @squared_root = Math.sqrt(@user_number_root)
     
     render("calculations/square_form_template.html.erb")
   end
-  
-  def square_root_results
-    
-    @user_number_root = params["square_root_number"].to_i
-    @squared_root = Math.sqrt(@user_number_root)
-    
-    render("calculations/square_results_template.html.erb")
-  end  
-  
-  
+
   
   def payment_form
     
@@ -110,27 +92,6 @@ class CalculationsController < ApplicationController
     render("calculations/payment_form_template.html.erb")
   end
   
-  def payment_results
-   
-    # interest_rate/years/present_value
-    
-    @monthly_interest_basispoints = (params["interest_rate"].to_i)
-    @number_of_years = (params["years"].to_i)
-    @principal = (params["principal"].to_i)
-    
-    
-    @number_of_monthly_payments = @number_of_years*-12
-    
-    @monthly_interest_percentage = @monthly_interest_basispoints/100
-    
-    @monthly_interest = @monthly_interest_percentage/12
-
-    @monthly_payment = @principal*(@monthly_interest/(1-(1+@monthly_interest)**@number_of_monthly_payments))
-    
-    render("calculations/payment_results_template.html.erb")
-  end
-  
-  
   
   def random_number_form
     
@@ -139,15 +100,6 @@ class CalculationsController < ApplicationController
     
     render("calculations/random_number_form_template.html.erb")
   end
-  
-  def random_number_results
-   
-    @random_number = params["random_number"].to_i
-    @random_number_output = rand(@minimum...@maximum)
-    
-    render("calculations/random_number_results_template.html.erb")
-  end
-  
   
   
   def word_count_form
@@ -172,14 +124,6 @@ class CalculationsController < ApplicationController
     
     render("calculations/word_count_form_template.html.erb")
   end
-  
-  def word_count_results
-   
-    
-    
-    render("calculations/word_count_results_template.html.erb")
-  end
-  
   
   
   def stats_form
@@ -216,39 +160,6 @@ class CalculationsController < ApplicationController
     render("calculations/stats_form_template.html.erb")
   end
   
-  def stats_results
-   
-    @stats_numbers = params[:list_of_numbers].gsub(',', '').split.map(&:to_f)
-     
-    @sorted_numbers = @numbers.sort
-
-    @count = @numbers.count
-
-    @minimum_stats = @numbers.min
-
-    @maximum_stats = @numbers.max
-
-    @range = @maximum-@minimum
-
-    @median = @sorted_numbers[((@count/2)+(1/2))]
-
-    @sum = @numbers.sum
-
-    @mean = @sum/@count
-
-    @demeaned=@numbers.map { |i| i - @mean}
-    @squared = @demeaned.map { |i| i**2}
-    @variance = @squared.sum / @count
-
-    @variance = @squared.sum / @count
-
-    @standard_deviation = Math.sqrt(@variance)
-
-    @mode_count = @numbers.map { |i| @numbers.count(i)}
-    @mode = @numbers[@mode_count.index(@mode_count.sort[@mode_count.count - 1])]
-    
-    render("calculations/stats_results_template.html.erb")
-  end
   
   
 end
