6b4fcf3fa9e75f62b25056e4f05bc831bc9d5715
Em Klick <eklick@chicagobooth.edu>
2017-07-18 18:22:42 +0000

    connections

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -62,7 +62,7 @@ class CalculationsController < ApplicationController
   
   def square_form
     
-    @user_number = params["number_input"].to_i
+    @user_number = params["the_user_number"].to_i
     @squared_number = @user_number**2
     
     render("calculations/square_form_template.html.erb")
@@ -80,9 +80,9 @@ class CalculationsController < ApplicationController
   
   def payment_form
     
-    @monthly_interest_basispoints = params["interest_input"].to_f
-    @number_of_years = params["years_input"].to_i
-    @present_value = params["principal_input"].to_i
+    @monthly_interest_basispoints = params["the_user_interest"].to_f
+    @number_of_years = params["the_user_payment_years"].to_i
+    @present_value = params["the_user_principal"].to_i
     
     @monthly_interest_percentage = @monthly_interest_basispoints/10000
     @monthly_interest_per_period = @monthly_interest_percentage/12
@@ -104,8 +104,8 @@ class CalculationsController < ApplicationController
   
   def random_number_form
     
-    @minimum = params["random_minimum_input"].to_i
-    @maximum = params["random_maximum_input"].to_i
+    @minimum = params["minimum"].to_f
+    @maximum = params["maximum"].to_f
     @random_number_output = rand(@minimum...@maximum)
     
     render("calculations/random_number_form_template.html.erb")
@@ -171,6 +171,120 @@ class CalculationsController < ApplicationController
   end
   
   
+  
+  
+  def process_square_form
+    
+    # The incoming parameters for this action look like {"the_user_number"} => "5"
+    # Rails stores that hash in a variable called params
+    
+    @user_number = params["the_user_number"].to_f
+    @squared_number = @user_number**2
+ 
+    render("calculations/process_square_form_template.html.erb")
+  end
+  
+  def process_square_root_form
+    
+    @user_number_root = params["the_user_number_square_root"].to_f
+    @squared_root = Math.sqrt(@user_number_root)
+    
+    render("calculations/process_square_root_form_template.html.erb")
+  end
+
+  
+  def process_payment_form
+    
+    @monthly_interest_basispoints = params["the_user_interest"].to_f
+    @number_of_years = params["the_user_payment_years"].to_i
+    @present_value = params["the_user_principal"].to_i
+    
+    @monthly_interest_percentage = @monthly_interest_basispoints/10000
+    @monthly_interest_per_period = @monthly_interest_percentage/12
+    
+    @number_of_monthly_payments = @number_of_years*-12
+    
+    @one_plus_rate_per_period = (1 + @monthly_interest_per_period)**@number_of_monthly_payments
+    
+    @denomonator_step_one = (1 - @one_plus_rate_per_period)
+    
+    @numerator = (@monthly_interest_per_period*@present_value).to_i
+    @denomonator = (@denomonator_step_one).to_i
+    
+    @monthly_payment = @numerator/(1-(1+@monthly_interest_per_period)**(@number_of_monthly_payments))
+    
+    render("calculations/process_payment_form_template.html.erb")
+  end
+  
+  
+  def process_random_number_form
+    
+    @minimum = params["minimum"].to_f
+    @maximum = params["maximum"].to_f
+    @random_number_output = rand(@minimum...@maximum)
+    
+    render("calculations/process_random_number_form_template.html.erb")
+  end
+  
+  
+  def process_word_count_form
+     
+    @text = params["word_count_input"]
+    @special_word = params["special_word_input"]
+
+    @word_count = @text.split(" ").length
+
+    @character_count_with_spaces = @text.length
+
+    @character_count_without_spaces = @text.length-@text.count(" ")
+
+    punctuation = @text.chomp(".")
+    
+    downcased_words = punctuation.downcase.split(" ")
+    
+    downcased_special = @special_word.downcase
+
+    @occurrences = downcased_words.count(downcased_special)
+     
+    
+    render("calculations/process_word_count_form_template.html.erb")
+  end
+  
+  
+  def process_stats_form
+     
+    @numbers = params["list_of_numbers_input"].gsub(',', '').split.map(&:to_f)
+     
+    @sorted_numbers = @numbers.sort
+
+    @count = @numbers.count
+
+    @minimum_stats = @numbers.min
+
+    @maximum_stats = @numbers.max
+
+    @range = @maximum_stats-@minimum_stats
+
+    @median = @sorted_numbers[((@count/2)+(1/2))]
+
+    @sum = @numbers.sum
+
+    @mean = @sum/@count
+
+    @demeaned=@numbers.map { |i| i - @mean}
+    @squared = @demeaned.map { |i| i**2}
+    @variance = @squared.sum / @count
+
+    @variance = @squared.sum / @count
+
+    @standard_deviation = Math.sqrt(@variance)
+
+    @mode_count = @numbers.map { |i| @numbers.count(i)}
+    @mode = @numbers[@mode_count.index(@mode_count.sort[@mode_count.count - 1])]
+    
+    render("calculations/process_stats_form_template.html.erb")
+  end
+ 
   
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -27,17 +27,22 @@ Rails.application.routes.draw do
   get("/descriptive_stats/new", {:controller => "calculations", :action => "stats_form"})
   
   
-  # get("/square/:user_number", { :controller => "calculations", :action => "square_form"})
   
-  # get("/square_root/:square_root_number", { :controller => "calculations", :action => "square_root_form"})
   
-  # get("/payment/:basis_points/:number_of_years/:present_value", { :controller => "calculations", :action => "payment_form"}) 
   
-  # get("/random/:min/:max", { :controller => "calculations", :action => "random_number_form"})
+  get("/square/results", { :controller => "calculations", :action => "process_square_form"})
   
-  # get("/word_count/:user_text/:special_word", {:controller => "calculations", :action => "word_count_form"})
+  get("/square_root/results", { :controller => "calculations", :action => "process_square_root_form"})
   
-  # get("/descriptive_stats/:list_of_numbers", {:controller => "calculations", :action => "stats_form"})
+  get("/payment/results", { :controller => "calculations", :action => "process_payment_form"}) 
+  
+  get("/random/results", { :controller => "calculations", :action => "process_random_number_form"})
+  
+  
+  get("/word_count/results", {:controller => "calculations", :action => "process_word_count_form"})
+  
+  get("/descriptive_stats/results", {:controller => "calculations", :action => "process_stats_form"})
+  
   
   
   
